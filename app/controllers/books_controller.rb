0688d5f054265fed7e3d230cfa0e4a781978777d
y-n-h-y <upboshi.bigboy@gmail.com>
2021-08-15 12:03:41 +0000

    fix2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,20 +1,21 @@
 class BooksController < ApplicationController
+
   def index
-    @books = Book.all
     @book = Book.new
+    @books = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
   end
 
-  def new
-  end
-
   def create
-    book = Book.new(book_params)
-    book.save
-    redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+    redirect_to book_path(@book.id)
+    else
+    render :new
+    end
   end
 
   def edit
